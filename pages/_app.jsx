78d992f3806c7756688a4850216eb1efd112e29c
data-miner00 <data-miner00@users.noreply.github.com>
2021-06-02 12:57:54 +0800

    feat: Apply styles and layouts

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "../styles/layout.css";
+import "../styles/home.sass";
+import "../styles/article.sass";
+import "../styles/preview.sass";
+import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
