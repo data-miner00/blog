304bf79a94f549df50b57b252b41895d9d07027b
data-miner00 <data-miner00@users.noreply.github.com>
2021-06-03 11:20:46 +0800

    refactor: Externalize utility func

--- components/PostPreview.jsx
+++ components/PostPreview.jsx
@@ -2,23 +2,19 @@ import { faNewspaper } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon as Fa } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import Image from "next/image";
-
-const LANGUAGES = {
-  en: "English",
-  jp: "日本語",
-  kr: "한국어",
-  ch: "中文",
-};
+import { getLanguage } from "../services/getLanguage";
+import { getDate } from "../services/getDate";
 
 export const PostPreview = ({ post }) => {
-  const { imgUrl, title, subtitle, slug, language, date, tags } = post;
+  const { image, title, subtitle, slug, language, tags } = post.fields;
+  const { createdAt } = post.sys;
 
   return (
     <Link href={`/article/${slug}`}>
       <div className="post-preview">
         <div className="post-preview__illus">
           <Image
-            src={imgUrl}
+            src={`https:${image.fields.file.url}`}
             alt="thumbnail of a post"
             layout="fill"
             objectFit="cover"
@@ -33,10 +29,10 @@ export const PostPreview = ({ post }) => {
           <div className="post-preview__details__subtitle">{subtitle}</div>
           <div className="row">
             <div className="post-preview__details__date">
-              <Fa icon={faNewspaper}></Fa> {date}
+              <Fa icon={faNewspaper}></Fa> {getDate(createdAt, 1)}
             </div>
             <div className="post-preview__details__language">
-              {LANGUAGES[language]}
+              {getLanguage(language)}
             </div>
           </div>
           <div className="post-preview__details__tags">
