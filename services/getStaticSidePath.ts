1d4052e70efde97b971edf0e23a639cb648ce27d
data-miner00 <data-miner00@users.noreply.github.com>
2022-10-08 21:39:01 +0800

    feat: Added injectable base url

--- services/getStaticSidePath.ts
+++ services/getStaticSidePath.ts
@@ -3,10 +3,13 @@ import { useRouter } from "next/router";
 export function getStaticSidePath(): string {
   const { asPath } = useRouter();
 
-  const origin =
-    typeof window !== "undefined" && window.location.origin
-      ? window.location.origin
-      : "";
+  const injectedBasePath = process.env.APP_BASE_URL;
+
+  const origin = injectedBasePath
+    ? injectedBasePath
+    : typeof window !== "undefined" && window.location.origin
+    ? window.location.origin
+    : "";
 
   const url = `${origin}${asPath}`;
 
