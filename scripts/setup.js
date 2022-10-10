a88bea8ccf7e33559ada91063f4c5d0cf6f8806f
data-miner00 <data-miner00@users.noreply.github.com>
2022-10-10 10:59:08 +0800

    feat: Create empty file before migration

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,9 +1,14 @@
 const sqlite = require("sqlite");
 const sqlite3 = require("sqlite3");
+const fs = require("fs");
 
 async function setup() {
+  const filename = "./data/db.sqlite";
+
+  fs.closeSync(fs.openSync(filename, "w"));
+
   const db = await sqlite.open({
-    filename: "./data/db.sqlite",
+    filename,
     driver: sqlite3.Database,
   });
   await db.migrate({ migrationsPath: "./migrations", force: "last" });
