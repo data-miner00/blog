5a101707ecd0864b6389b64f184d29725f009a41
data-miner00 <data-miner00@users.noreply.github.com>
2022-10-10 19:58:24 +0800

    fix: Added try catch

--- pages/api/cheers/index.ts
+++ pages/api/cheers/index.ts
@@ -8,12 +8,16 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     return;
   }
 
-  const db = await sqlite.open({
-    filename: "./data/db.sqlite",
-    driver: sqlite3.Database,
-  });
+  try {
+    const db = await sqlite.open({
+      filename: "./data/db.sqlite",
+      driver: sqlite3.Database,
+    });
 
-  const cheers = await db.all("SELECT * FROM Cheers;");
+    const cheers = await db.all("SELECT * FROM Cheers;");
 
-  res.status(200).json({ data: cheers });
+    res.status(200).json({ data: cheers });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 };
