0f199587a0f5546d2afcf799263912613f94e71f
data-miner00 <data-miner00@users.noreply.github.com>
2022-12-13 01:09:40 +0800

    style: Update Skeleton

--- components/Skeleton.jsx
+++ components/Skeleton.jsx
@@ -4,59 +4,48 @@ export default function Skeleton() {
   return (
     <div className="loading">
       <Header />
-      <div className="loading__img"></div>
-      <div className="loading__title"></div>
-      <div className="loading__title2"></div>
-      <div className="loading__user"></div>
-      <div className="loading__content"></div>
-      <div className="loading__content2"></div>
-      <div className="loading__content3"></div>
+
+      {/* img */}
+      <div className="w-full h-[60vh] bg-gray-700"></div>
+
+      {/* content */}
+      <div className="px-[80px]">
+        <div className="bg-gray-700 w-[70%] h-16 mt-12"></div>
+        <div className="h-8 bg-gray-700 w-full mt-6"></div>
+        <div className="h-8 bg-gray-700 w-[30%] mt-3"></div>
+        <div className="flex justify-between my-12 items-center">
+          <div className="flex">
+            <div className="h-12 w-12 rounded-full bg-gray-700"></div>
+            <div className="ml-8">
+              <div className="bg-gray-700 w-80 h-5 mb-2"></div>
+              <div className="bg-gray-700 w-80 h-5"></div>
+            </div>
+          </div>
+          <div className="h-10 bg-gray-700 w-72"></div>
+        </div>
+        <div className="mb-12">
+          <div className="h-5 w-full bg-gray-700 mb-2"></div>
+          <div className="h-5 w-[30%] bg-gray-700 mb-2"></div>
+          <div className="h-5 w-[60%] bg-gray-700"></div>
+        </div>
+        <div className="mb-12">
+          <div className="h-5 w-full bg-gray-700 mb-2"></div>
+          <div className="h-5 w-[30%] bg-gray-700 mb-2"></div>
+          <div className="h-5 w-full bg-gray-700 mb-2"></div>
+          <div className="h-5 w-[60%] bg-gray-700 mb-2"></div>
+          <div className="h-5 w-[30%] bg-gray-700 mb-2"></div>
+        </div>
+        <div>
+          <div className="h-5 w-full bg-gray-700 mb-2"></div>
+          <div className="h-5 w-[30%] bg-gray-700 mb-2"></div>
+          <div className="h-5 w-[60%] bg-gray-700 mb-2"></div>
+          <div className="h-5 w-full bg-gray-700 mb-2"></div>
+        </div>
+      </div>
 
       <style jsx>{`
         .loading {
-          padding: 100px 20%;
-          height: 120vh;
-        }
-        .loading__img {
-          width: 100%;
-          height: 300px;
-          background: gray;
-        }
-        .loading__title {
-          height: 40px;
-          width: 70%;
-          background: gray;
-          margin-top: 20px;
-        }
-        .loading__title2 {
-          height: 40px;
-          width: 50%;
-          background: gray;
-          margin-top: 20px;
-        }
-        .loading__user {
-          height: 50px;
-          width: 100%;
-          background: gray;
-          margin-top: 40px;
-        }
-        .loading__content {
-          height: 20px;
-          width: 70%;
-          background: gray;
-          margin-top: 40px;
-        }
-        .loading__content2 {
-          height: 20px;
-          width: 50%;
-          background: gray;
-          margin-top: 25px;
-        }
-        .loading__content3 {
-          height: 20px;
-          width: 100%;
-          background: gray;
-          margin-top: 25px;
+          padding: 150px calc(20% + 50px) 50px;
         }
       `}</style>
     </div>
