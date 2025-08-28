3ef7da5944bb3c5897d9200dab3bf5abbad04d9f
Shaun Chong <data-miner00@users.noreply.github.com>
2025-08-29 05:17:33 +0800

    feat: Encode url txts

--- services/getShareUrl.ts
+++ services/getShareUrl.ts
@@ -11,17 +11,20 @@ export function getShareUrl(
   urlToShare: string,
   title: string
 ): string {
+  const encodedUrl = encodeURIComponent(urlToShare);
+  const encodedTitle = encodeURIComponent(title);
+
   const twitterUrls = {
-    v1: `https://twitter.com/intent/tweet?url=${urlToShare}&text=${title}`,
-    v2: `https://twitter.com/share?url=${urlToShare}&text=${title}`,
+    v1: `https://twitter.com/intent/tweet?url=${encodedUrl}&text=${encodedTitle}`,
+    v2: `https://twitter.com/share?url=${encodedUrl}&text=${encodedTitle}`,
   };
 
   const urls = {
     [SocialMedia.Twitter]: twitterUrls["v1"],
-    [SocialMedia.Facebook]: `https://www.facebook.com/sharer/sharer.php?u=${urlToShare}`,
-    [SocialMedia.LinkedIn]: `https://www.linkedin.com/shareArticle?mini=true&url=${urlToShare}&title=${title}`,
-    [SocialMedia.HackerNews]: `https://www.ycombinator.com/submitlink?u=${urlToShare}&t=${title}`,
-    [SocialMedia.Reddit]: `https://www.reddit.com/submit?url=${urlToShare}&title=${title}`,
+    [SocialMedia.Facebook]: `https://www.facebook.com/sharer/sharer.php?u=${encodedUrl}`,
+    [SocialMedia.LinkedIn]: `https://www.linkedin.com/shareArticle?mini=true&url=${encodedUrl}&title=${encodedTitle}`,
+    [SocialMedia.HackerNews]: `https://www.ycombinator.com/submitlink?u=${encodedUrl}&t=${encodedTitle}`,
+    [SocialMedia.Reddit]: `https://www.reddit.com/submit?url=${encodedUrl}&title=${encodedTitle}`,
   };
 
   return urls[socialMedia];
